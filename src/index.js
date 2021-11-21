18415ac1702baac2b3bcb557d65a09cc922509ca
SreejaPaul41 <sreeja.paul@s759labs.com>
2021-11-21 23:17:04 +0530

    route fix

--- src/index.js
+++ src/index.js
@@ -7,7 +7,7 @@ import {BrowserRouter} from 'react-router-dom';
 
 ReactDOM.render(
   <React.StrictMode>
-    <BrowserRouter basepath="/">
+    <BrowserRouter basepath="/Movie-Information">
       <App />
     </BrowserRouter>
     
